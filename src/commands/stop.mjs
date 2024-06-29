5084eb2a8229a2d7dd034150177bd85b03b08ee6
ToddyTheNoobDud <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-06-29 11:38:19 -0300

    Delete src/commands directory

--- src/commands/stop.mjs
+++
@@ -1,9 +0,0 @@
-export const Command = {
-    name: "stop",
-    description: "Stop the music!",
-    run: async (client, interaction) => {
-        const vc = interaction.member?.voice?.channel;
-        if (!vc) return;
-        await client.distube.stop(vc)
-    }
-}
