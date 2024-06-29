d0ccb80cd12616d45160c12085040dc8498d2517
ToddyTheNoobDud <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-06-29 17:29:10 -0300

    handle errors

--- src/commands/stop.mjs
+++ src/commands/stop.mjs
@@ -1,3 +1,5 @@
+import { ChannelType } from "discord.js";
+
 export const Command = {
     name: "stop",
     description: "Stop the music!",
@@ -5,9 +7,26 @@ export const Command = {
         try {
             const vc = interaction.member?.voice?.channel;
             if (!vc) return;
-            await client.distube.stop(vc)
-        } catch(error) {
-            console.log(error)
-        }
+
+      const { guild, channel } = interaction;
+
+      const lol = guild.channels.cache
+        .filter((chnl) => chnl.type == ChannelType.GuildVoice)
+        .find((channel) => channel.members.has(client.user.id));
+      if (lol && vc.id !== lol.id)
+        return interaction.reply({
+          content: `im already on <#${lol.id}>`,
+          ephemeral: true,
+        });
+
+      await client.distube.stop(vc);
+      await client.distube.voices.leave(vc)
+      await interaction.reply({
+        content: "⏹ Stopped",
+        ephemeral: true,
+      });
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
+};
