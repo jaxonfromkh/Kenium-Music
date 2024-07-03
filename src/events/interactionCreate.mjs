2c6ecdbebb4a3468a5256f2aed9c239e278643f8
ToddyTheNoobDud <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-07-03 12:29:21 -0300

    error handler

--- src/events/interactionCreate.mjs
+++ src/events/interactionCreate.mjs
@@ -1,21 +1,31 @@
 export const Event = {
     name: "interactionCreate",
-    run: async(client, interaction) => {
-        switch (true) {
-            case interaction.isChatInputCommand():
-                const command = client.slashCommands.get(interaction.commandName);
-                if (command) command.run(client, interaction);
-                break;
-            case interaction.isButton():
-                const button = client.buttonCommands.get(interaction.customId);
-                if (button) button.run(client, interaction);
-                break;
-            case interaction.isStringSelectMenu():
-                const selectMenuCommand = client.selectMenus.get(interaction.values[0]) ?? client.selectMenus.get(interaction.customId);
-                if (!selectMenuCommand) return;
+    run: async (client, interaction) => {
+        try {
+            switch (true) {
+                case interaction.isChatInputCommand():
+                    const command = client.slashCommands.get(interaction.commandName);
+                    if (command) command.run(client, interaction);
+                    break;
+                case interaction.isButton():
+                    const button = client.buttonCommands.get(interaction.customId);
+                    if (button) button.run(client, interaction);
+                    break;
+
+                case interaction.isStringSelectMenu():
+                    const selectMenuCommand = client.selectMenus.get(interaction.values[0]) ?? client.selectMenus.get(interaction.customId);
+                    if (!selectMenuCommand) return;
                 
-                selectMenuCommand.run(interaction, client);
-                break;
+                    selectMenuCommand.run(interaction, client);
+                    break;
+            }
+        } catch (error) {
+            if (error instanceof DiscordAPIError && error.code === 10062) {
+                console.warn(`DiscordAPIError[10062]: Unknown interaction ${interaction.id}`);
+            } else {
+                console.error(error);
+            }
         }
     }
 }
+
