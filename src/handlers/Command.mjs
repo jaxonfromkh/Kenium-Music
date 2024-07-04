2d2664660d53cfe0fbcfb150e2f1f034fa04327b
ToddyTheNoobDud <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-07-04 12:46:52 -0300

    Experimental: Improved Speed and Memory Usage

--- src/handlers/Command.mjs
+++ src/handlers/Command.mjs
@@ -1,33 +1,39 @@
-import { ApplicationCommandOptionType, REST, Routes } from "discord.js";
-import { Filereader } from "./filereader.mjs";
+import { REST, Routes } from "discord.js";
+import { token, id } from "../../config.mjs";
 import { pathToFileURL } from "url";
-import { token, id } from "../../config.mjs"
+import { Filereader } from "./filereader.mjs";
 
+export const CommandHandler = async (client, rootPath) => {
 
-export const CommandHandler = async (client, roothPath) => {
-    const AllFiles = await Filereader(`${roothPath}/src/commands`);
+    const AllFiles = await Filereader(`${rootPath}/src/commands`)
+
     const rest = new REST({ version: "10" }).setToken(token);
-    if (AllFiles.length > 0) {
-        const commandsPromises = AllFiles.map(async (CommandFile) => {
-            const { Command } = await import(pathToFileURL(CommandFile));
-            if (Command && !Command.ignore && Command.name && Command.description) {
-                client.slashCommands?.set(Command.name, Command);
-                return {    
-                    name: Command.name,
-                    nsfw: Command.nsfw ?? false,    
-                    description: Command.description,
-                    type: ApplicationCommandOptionType.Subcommand,
-                    options: Command.options ?? []
-                };
-            }
-        });
-        const CommandsArray = (await Promise.all(commandsPromises)).filter(Boolean);
-        try {   
-            console.log("Started refreshing application (/) commands.");
-            await rest.put(Routes.applicationCommands(id), { body: CommandsArray });
-            console.log("Successfully reloaded application (/) commands.");
-        } catch (error) {   
-            console.error(error);
+    const commands = await Promise.all(AllFiles.map(async (CommandFile) => {
+        const { Command } = await import(pathToFileURL(CommandFile));
+        if (Command && !Command.ignore && Command.name && Command.description) {
+            client.slashCommands?.set(Command.name, Command);
+            return {
+                name: Command.name,
+                nsfw: Command.nsfw ?? false,
+                description: Command.description,
+                type: 1,
+                options: Command.options ?? []
+            };
         }
+        return null;
+    }));
+
+    const CommandsArray = commands.filter(Boolean);
+
+    try {
+        console.log("Started refreshing application (/) commands.");
+        await rest.put(
+            Routes.applicationCommands(id),
+            { body: CommandsArray.flat() }
+        );
+        console.log("Successfully reloaded application (/) commands.");
+
+    } catch (error) {
+        console.error(error);
     }
-}
+};
