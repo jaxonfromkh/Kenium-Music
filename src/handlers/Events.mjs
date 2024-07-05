021b05d652d70fa7e059b9aa21c9390b4b5f20a3
ToddyTheNoobDud <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-07-05 09:20:16 -0300

    Support for JS (and MJS) + Optimize

--- src/handlers/Events.mjs
+++ src/handlers/Events.mjs
@@ -1,32 +1,24 @@
-import { Filereader } from "./filereader.mjs";
-import { pathToFileURL } from "url";
-
+import { readdir } from "fs/promises";
+import { join, extname } from "path";
+import { fileURLToPath } from "url";
 
 export const EventHandler = async (client, rootPath) => {
-    const eventFiles = Filereader(`${rootPath}/src/events`);
-    if (!eventFiles?.length) return; // check for null or empty array before proceeding
+    const eventsDir = join(rootPath, "src", "events");
+    const eventFiles = await readdir(eventsDir, { withFileTypes: true });
 
-    const promises = [];
-    for (const event of eventFiles) {
-        promises.push(
-            (async () => {
-                try {
-                    const { Event: clientEvent } = await import(pathToFileURL(event)) || {};
-                    if (clientEvent) {
-                        client.events?.set(clientEvent.name, clientEvent);
-                        if (!clientEvent.ignore) {
-                            if (clientEvent.customEvent) clientEvent.run(client);
-                            else if (clientEvent.runOnce) client.once(clientEvent.name, (...args) => clientEvent.run(client, ...args));
-                            else client.on(clientEvent.name, (...args) => clientEvent.run(client, ...args));
-                        }
-                    }
+    for (const eventFile of eventFiles) {
+        if (eventFile.isFile() && (extname(eventFile.name) === ".js" || extname(eventFile.name) === ".mjs")) {
+            const eventPath = join(eventsDir, eventFile.name);
+            const { Event: clientEvent } = await import(new URL(`file://${eventPath}`, import.meta.url));
+            if (clientEvent) {
+                client.events?.set(clientEvent.name, clientEvent);
+                if (!clientEvent.ignore) {
+                    const eventFunction = clientEvent.customEvent ? clientEvent.run.bind(null, client) : clientEvent.run.bind(null, client);
+                    if (clientEvent.runOnce) client.once(clientEvent.name, eventFunction);
+                    else client.on(clientEvent.name, eventFunction);
                 }
-                catch (error) {
-                    console.error(error);
-                    throw error; // re-throw the error to see the stack trace
-                }
-            })()
-        );
+            }
+        }
     }
-    await Promise.allSettled(promises);
 }
+
