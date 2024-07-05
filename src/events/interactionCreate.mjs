a2ce00fd28a8a53b1642d4808eb450f1c045a81e
ToddyTheNoobDud <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-07-05 09:19:43 -0300

    Faster + Optimize

--- src/events/interactionCreate.mjs
+++ src/events/interactionCreate.mjs
@@ -2,29 +2,13 @@ export const Event = {
     name: "interactionCreate",
     run: async (client, interaction) => {
         try {
-            switch (true) {
-                case interaction.isChatInputCommand():
-                    const command = client.slashCommands.get(interaction.commandName);
-                    if (command) command.run(client, interaction);
-                    break;
-                case interaction.isButton():
-                    const button = client.buttonCommands.get(interaction.customId);
-                    if (button) button.run(client, interaction);
-                    break;
+            const command = interaction.isChatInputCommand() ? client.slashCommands.get(interaction.commandName) :
+            interaction.isButton() ? client.buttonCommands.get(interaction.customId) :
+            interaction.isStringSelectMenu() ? client.selectMenus.get(interaction.values[0] ?? interaction.customId) : null;
 
-                case interaction.isStringSelectMenu():
-                    const selectMenuCommand = client.selectMenus.get(interaction.values[0]) ?? client.selectMenus.get(interaction.customId);
-                    if (!selectMenuCommand) return;
-                
-                    selectMenuCommand.run(interaction, client);
-                    break;
-            }
+            if (command) command.run(client, interaction);
         } catch (error) {
-            if (error instanceof DiscordAPIError && error.code === 10062) {
-                console.warn(`DiscordAPIError[10062]: Unknown interaction ${interaction.id}`);
-            } else {
-                console.error(error);
-            }
+            console.log(error);
         }
     }
 }
