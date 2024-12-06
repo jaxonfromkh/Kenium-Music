b6b1800225dc3a171730b151bf4fafdd908932e0
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-12-06 10:45:25 -0300

    rewrite

--- src/commands/pause.mjs
+++ src/commands/pause.mjs
@@ -1,35 +1,19 @@
-import { ChannelType } from "discord.js";
-
 export const Command = {
     name: "pause",
     description: "Pause the current playing song",
 
     run: async (client, interaction) => {
-        const vc = interaction.member?.voice?.channel;
-        if (!vc) return;
-        const player = client.aqua.players.get(interaction.guildId)
-        if (!player) {
-            return interaction.reply({ content: "Nothing is playing", ephemeral: true });
-        }
-        if (player.paused) {
-            return interaction.reply({ content: "The player is already paused", ephemeral: true });
-        }
-        const { guild, channel } = interaction;
-  
-        const lol = guild.channels.cache
-          .filter((chnl) => chnl.type == ChannelType.GuildVoice)
-          .find((channel) => channel.members.has(client.user.id));
-        if (lol && vc.id !== lol.id)
-          return interaction.reply({
-            content: `im already on <#${lol.id}>`,
-            ephemeral: true,
-          });
-          
+        const { guild, member } = interaction;
+        const player = client.aqua.players.get(guild.id);
+        if (!player || !member.voice.channel) return;
+
+        if (interaction.guild.members.me.voice.channelId !== interaction.member.voice.channelId) return;
+
         player.pause(true);
 
         return interaction.reply({
             content: 'Paused the song',
             ephemeral: true
         })
-        }
+    }
 }
