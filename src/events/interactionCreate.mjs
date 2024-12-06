fc219564440b0b677b31b0e50a08b32780b35402
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-12-06 10:42:34 -0300

    idk

--- src/events/interactionCreate.mjs
+++ src/events/interactionCreate.mjs
@@ -1,14 +1,15 @@
 export const Event = {
     name: "interactionCreate",
     run: async (client, interaction) => {
-        let command;
         try {
+            let command;
+            
             if (interaction.isChatInputCommand()) {
                 command = client.slashCommands?.get(interaction.commandName);
             } else if (interaction.isButton()) {
                 command = client.buttonCommands.get(interaction.customId);
             } else if (interaction.isStringSelectMenu()) {
-                command = client.selectMenus.get(interaction.values[0] ?? interaction.customId);
+                command = client.selectMenus.get(interaction.customId) || client.selectMenus.get(interaction.values[0]);
             }
 
             if (command) {
@@ -16,10 +17,6 @@ export const Event = {
             }
         } catch (error) {
             console.error("Error handling interaction:", error);
-        } finally {
-            if (global.gc) {
-                global.gc();
-            }
         }
     }
 };
