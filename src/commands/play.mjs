128bc7d12b1b92c2d8ee274de99d7b3bcff87911
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-12-06 10:45:14 -0300

    rewrite

--- src/commands/play.mjs
+++ src/commands/play.mjs
@@ -14,23 +14,19 @@ export const Command = {
 
   run: async (client, interaction) => {
     try {
-      const vc = interaction.member.voice.channel;
+      const { guild, member } = interaction;
+      const vc = member.voice.channel;
       if (!vc) return;
 
-      const { guild, channel } = interaction;
-
-      const lol = guild.channels.cache
-        .filter((chnl) => chnl.type == ChannelType.GuildVoice)
-        .find((channel) => channel.members.has(client.user.id));
-      if (lol && vc.id !== lol.id)
+      const lol = guild.channels.cache.find(
+        (chnl) => chnl.type === ChannelType.GuildVoice && chnl.members.has(client.user.id)
+      );
+      if (lol && vc.id !== lol.id) {
         return interaction.reply({
           content: `im already on <#${lol.id}>`,
           ephemeral: true,
         });
-
-      if (vc.full) return interaction.reply({ content: "I can't join this vc because it's full", ephemeral: true });
-
-
+      }
       const query = interaction.options.getString('query');
 
       const player = client.aqua.createConnection({
@@ -42,41 +38,36 @@ export const Command = {
 
       const result = await client.aqua.resolve({ query, requester: interaction.member });
       const embed = new EmbedBuilder().setColor(0x000000)
-      const track = result.tracks.shift();
+      const tracks = result.tracks;
       
       switch (result.loadType) {
         case "track":
-          player.queue.add(track);
+          player.queue.add(tracks[0]);
           embed.setDescription(
-            `Added [${track.info.title}](${track.info.uri}) to the queue.`
+            `Added [${tracks[0].info.title}](${tracks[0].info.uri}) to the queue.`
           );
-          await interaction.reply({ embeds: [embed], ephemeral: true });
-
-          if (!player.playing && !player.paused && player.queue.size > 0) return player.play();
-
+          break;
+        case "search":
+          player.queue.add(tracks.shift());
+          embed.setDescription(
+            `Added [${tracks[0].info.title}](${tracks[0].info.uri}) to the queue.`
+          );
+          break;
         case "playlist":
-          for (const track of result.tracks) {
+          for (const track of tracks) {
             track.info.requester = interaction.member;
             player.queue.add(track);
           }
 
-          const playlistInfo = result.data;
           embed.setDescription(
-            `Added [${playlistInfo.name}](${query}) playlist to the queue.`
+            `Added [${result.playlistInfo.name}](${query}) playlist to the queue.`
           );
-          await interaction.reply({ embeds: [embed], ephemeral: true });
-          if (!player.playing && !player.paused && player.queue.size > 0) return player.play();
           break;
-
-        case "search":
-          player.queue.add(track);
-          embed.setDescription(
-            `Added [${track.info.title}](${track.info.uri}) to the queue.`
-          );
-
-          await interaction.reply({ embeds: [embed], ephemeral: true });
-          if (!player.playing && !player.paused && player.queue.size > 0) return player.play();
       }
+
+      await interaction.reply({ embeds: [embed], ephemeral: true });
+
+      if (!player.playing && !player.paused && player.queue.size > 0) player.play();
     } catch (error) {
       console.log(error);
     }
