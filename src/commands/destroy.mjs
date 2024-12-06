e16086142fa2773fe61c3119e7931a5d42ca2338
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-12-06 10:46:18 -0300

    Add destroy

---
+++ src/commands/destroy.mjs
@@ -0,0 +1,16 @@
+export const Command = {
+    name: "destroy",
+    description: 'destroy the music',
+    options: [],
+    run: async (client, interaction) => {
+        const player = client.aqua.players.get(interaction.guildId);
+        if (!player || !interaction.member?.voice?.channel) return;
+        if (interaction.guild.members.me.voice.channelId !== interaction.member.voice.channelId) return;
+        
+        player.destroy()
+        await interaction.reply({
+            content: "Destroyed the music",
+            ephemeral: true,
+        })
+    }
+}
