646a9d536b03d75bef07dfb8082026fa21a335f3
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-12-06 10:45:51 -0300

    rewrite

--- src/commands/export.mjs
+++ src/commands/export.mjs
@@ -5,37 +5,21 @@ export const Command = {
     description: "Export the queue",
     options: [],
     run: async (client, interaction) => {
-        try {
-            const player = client.aqua.players.get(interaction.guildId);
-            if (!player) {
-                return interaction.reply({ content: "Nothing is playing", ephemeral: true });
-            }
-
-            if (player.queue.length === 0) {
-                return interaction.reply({ content: "Queue is empty", ephemeral: true });
-            }
-
-            const queue = player.queue.map((track) => {
-                return `${track.info.uri}`;
-            }).join('\n');
-
-            const attachment = new AttachmentBuilder(Buffer.from(queue), { name: `ToddysMusicV2.3.0.txt` });
-            await interaction.reply({ files: [attachment], content: 'This will be Deleted after 1 minute!\n Pro tip: You can use </import:1305541038496153660> to import the queue' });
-            
-            const timeoutId = setTimeout(async () => {
-                try {
-                    await interaction.deleteReply();
-                } catch (err) {
-                    console.error("Failed to delete interaction reply:", err);
-                }
-            }, 60000);
-
-        } catch (error) {
-            if (error.code !== 10008) {
-                console.error("An error occurred while processing the command:", error);
-                throw error; // Only throw if it's not a specific error
-            }
-            console.log(error); // Log any expected errors
+        const player = client.aqua.players.get(interaction.guildId);
+        if (!player || player.queue.length === 0) {
+            return interaction.reply({ content: player ? "Queue is empty" : "Nothing is playing", ephemeral: true });
         }
+        if (interaction.guild.members.me.voice.channelId !== interaction.member.voice.channelId) return;
+        const attachment = new AttachmentBuilder(Buffer.from(player.queue.map(track => track.info.uri).join('\n')), { name: `ToddysMusicV2.4.0.txt` });
+        const reply = await interaction.reply({ files: [attachment], content: 'This will be Deleted after 1 minute!\n Pro tip: You can use </import:1305541038496153660> to import the queue' });
+        
+        setTimeout(async () => {
+            try {
+                await reply.delete();
+            } catch (err) {
+                console.error("Failed to delete interaction reply:", err);
+            }
+        }, 60000);
+
     }
 };
