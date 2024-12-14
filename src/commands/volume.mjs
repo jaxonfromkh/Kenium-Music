ffdd91832e2c9a63d2e3ec4eae19e4102e2663da
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-12-13 22:44:40 -0300

    small optimization

--- src/commands/volume.mjs
+++ src/commands/volume.mjs
@@ -1,45 +1,38 @@
-import { ChannelType, EmbedBuilder } from "discord.js";
+import {  EmbedBuilder } from "discord.js";
 
 export const Command = {
-    name: "volume",
-    description: "Want some volume bro?",
-    options: [
-        {
-          name: "volume",
-          description: "Enter the volume amount to set",
-          type: 4,
-          required: true,
-        },
+  name: "volume",
+  description: "Want some volume bro?",
+  options: [
+    {
+      name: "volume",
+      description: "Enter the volume amount to set",
+      type: 4,
+      required: true,
+    },
+  ],
+  run: async (client, interaction) => {
+    const player = client.aqua.players.get(interaction.guildId);
+    if (!player || !interaction.member.voice.channel || interaction.guild.members.me.voice.channelId !== interaction.member.voice.channelId) return;
+    const volume = interaction.options.getInteger("volume", true);
+    if (volume < 0 || volume > 150) {
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor("Red")
+            .setDescription(`Use an number between \`0 - 150\`.`),
+        ],
+      });
+    }
+    player.setVolume(volume);
+    return interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor(0x000000)
+          .setDescription(
+            `Volume is now set to **${player.volume}%**`
+          ),
       ],
-
-    run: async (client, interaction) => {
-        const player = client.aqua.players.get(interaction.guildId);
-        if (!player || !interaction.member?.voice?.channel) return interaction.reply({ content: "Nothing is playing", ephemeral: true });
-        const { guild } = interaction;
-        const vc = guild.channels.cache.get(player.voiceChannel);
-        if (vc?.id !== interaction.member.voice.channelId)
-          return interaction.reply({ content: `im already on <#${vc.id}>`, ephemeral: true });
-        const volume = interaction.options.getInteger("volume", true);
-        if (volume < 0 || volume > 150) {
-            return interaction.reply({
-              embeds: [
-                new EmbedBuilder()
-                  .setColor("Red")
-                  .setDescription(`Use an number between \`0 - 150\`.`),
-              ],
-            });
-          }
-          player.setVolume(volume);
-
-          return interaction.reply({
-            embeds: [
-              new EmbedBuilder()
-                .setColor(0x000000)
-                .setDescription(
-                  `Volume is now set to **${player.volume}%**`
-                ),
-            ],
-          });
-        }
-        }
-        
+    });
+  }
+}
