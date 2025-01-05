6fb22fd61febd742887eabbc82565850dac92b79
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-01-05 19:12:48 -0300

    idk why

--- src/commands/ping.mjs
+++ src/commands/ping.mjs
@@ -1,4 +1,4 @@
-import { EmbedBuilder } from 'discord.js'
+import { EmbedBuilder } from 'discord.js';
 
 export const Command = {
     name: "ping",
@@ -6,27 +6,20 @@ export const Command = {
     run: async (client, interaction) => {
         try {
             const apiPing = client.ws.ping;
-            const ping = Date.now() - interaction.createdTimestamp;
+            const botPing = Date.now() - interaction.createdTimestamp;
+
             const embed = new EmbedBuilder()
                 .setColor(0x000000)
                 .setTitle("Pong!")
-                .addFields([
-                    {
-                        name: "API Ping",
-                        value: `\`${apiPing}\` ms`,
-                        inline: true,
-                    },
-                    {
-                        name: "Bot Ping",
-                        value: `\`${ping}\` ms`,
-                        inline: true,
-                    },
-                ])
-                .setFooter({ text: "Made by mushroom0162" })
+                .addFields(
+                    { name: "API Ping", value: `\`${apiPing}\` ms`, inline: true },
+                    { name: "Bot Ping", value: `\`${botPing}\` ms`, inline: true }
+                )
+                .setFooter({ text: "Made by mushroom0162" });
+
             await interaction.reply({ embeds: [embed] });
         } catch (error) {
-            console.log(error)
+            console.error(error);
         }
     }
-}
-
+};
