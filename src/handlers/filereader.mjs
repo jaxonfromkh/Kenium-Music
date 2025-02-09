d0e855083594fe2a4267420573571f5328b2ff6f
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-02-09 14:54:36 -0300

    Rewrite

--- src/handlers/filereader.mjs
+++ src/handlers/filereader.mjs
@@ -1,24 +1,21 @@
 import { readdir } from 'fs/promises';
 import { join, extname } from 'path';
 
-export const Filereader = async (dir) => {
-  const files = [];
-  
-  try {
-    const directoryData = await readdir(dir, { withFileTypes: true });
-
-    for (const entry of directoryData) {
-      const filePath = join(dir, entry.name);
-      if (entry.isDirectory()) {
-        const subFiles = await Filereader(filePath);
-        files.push(...subFiles);
-      } else if (extname(entry.name) === '.mjs') {
-        files.push(filePath);
-      }
+export async function* FilereaderGenerator(dir) {
+    let entries;
+    try {
+        entries = await readdir(dir, { withFileTypes: true });
+    } catch (error) {
+        console.error(`Failed to read directory: ${dir}`, error);
+        return;
     }
-  } catch (error) {
-    console.error(`Failed to read directory: ${dir}`, error);
-  }
-
-  return files;
-};
+    
+    for (const entry of entries) {
+        const filePath = join(dir, entry.name);
+        if (entry.isDirectory()) {
+            yield* FilereaderGenerator(filePath);
+        } else if (extname(entry.name) === '.mjs') {
+            yield filePath;
+        }
+    }
+}
