c0c63a5cbc12afd0a531e661e0d0f696ba01e7fa
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-02-17 17:40:33 -0300

    Misc optimizations

--- src/commands/export.mjs
+++ src/commands/export.mjs
@@ -1,59 +1,58 @@
 import { AttachmentBuilder } from "discord.js";
 
 const PLATFORMS = {
-    YOUTUBE: 'youtube',
-    SOUNDCLOUD: 'soundcloud',
-    SPOTIFY: 'spotify',
+  YOUTUBE: "youtube",
+  SOUNDCLOUD: "soundcloud",
+  SPOTIFY: "spotify",
 };
 
 export const Command = {
-    name: "export",
-    description: "Export the queue",
-    options: [],
-    run: async (client, interaction) => {
-        const player = client.aqua.players.get(interaction.guildId);
-        
-        if (!player || player.queue.length === 0) {
-            return interaction.reply({ content: player ? "Queue is empty" : "Nothing is playing", flags: 64 });
-        }
-
-        if (interaction.guild.members.me.voice.channelId !== interaction.member.voice.channelId) {
-            return interaction.reply({ content: "You must be in the same voice channel to use this command.", flags: 64 });
-        }
-
-        const tracks = player.queue.map(track => track.info.uri);
-        let fileName = 'Kenium_2.7.0';
-
-        let hasYouTube = false, hasSoundCloud = false, hasSpotify = false;
-
-        for (const uri of tracks) {
-            if (uri.includes(PLATFORMS.YOUTUBE)) hasYouTube = true;
-            if (uri.includes(PLATFORMS.SOUNDCLOUD)) hasSoundCloud = true;
-            if (uri.includes(PLATFORMS.SPOTIFY)) hasSpotify = true;
-        }
-
-        if (hasYouTube) fileName += '_youtube';
-        if (hasSoundCloud) fileName += '_soundcloud';
-        if (hasSpotify) fileName += '_spotify';
-        fileName += '.txt';
-
-        const attachment = new AttachmentBuilder(Buffer.from(tracks.join('\n')), { name: fileName });
-        
-        const reply = await interaction.reply({
-            files: [attachment],
-            content: 'This will be Deleted after 1 minute!\n Pro tip: You can use </import:1305541038496153660> to import the queue'
-        });
-
-        const deleteTimeout = setTimeout(async () => {
-            try {
-                await reply.delete();
-            } catch (err) {
-                console.error("Failed to delete interaction reply:", err);
-            }
-        }, 60000);
-
-        interaction.client.on('interactionDelete', () => {
-            clearTimeout(deleteTimeout);
-        });
+  name: "export",
+  description: "Export the queue",
+  options: [],
+  run: async (client, interaction) => {
+    const player = client.aqua.players.get(interaction.guildId);
+    if (!player) {
+      return interaction.reply({ content: "Nothing is playing", flags: 64 });
     }
+    if (player.queue.length === 0) {
+      return interaction.reply({ content: "Queue is empty", flags: 64 });
+    }
+
+    const botVcId = interaction.guild.members.me.voice.channelId;
+    const userVcId = interaction.member.voice.channelId;
+    if (botVcId !== userVcId) {
+      return interaction.reply({ content: "You must be in the same voice channel to use this command.", flags: 64 });
+    }
+
+    const trackURIs = player.queue.map((track) => track.info.uri);
+    let fileName = "Kenium_2.8.0";
+
+    if (trackURIs.some((uri) => uri.includes(PLATFORMS.YOUTUBE))) {
+      fileName += "_youtube";
+    }
+    if (trackURIs.some((uri) => uri.includes(PLATFORMS.SOUNDCLOUD))) {
+      fileName += "_soundcloud";
+    }
+    if (trackURIs.some((uri) => uri.includes(PLATFORMS.SPOTIFY))) {
+      fileName += "_spotify";
+    }
+    fileName += ".txt";
+
+    const attachment = new AttachmentBuilder(Buffer.from(trackURIs.join("\n")), { name: fileName });
+    const replyMessage = await interaction.reply({
+      files: [attachment],
+      content: "This will be deleted after 1 minute!\nPro tip: Use </import:1305541038496153660> to import the queue",
+    });
+
+    const deleteTimeout = setTimeout(async () => {
+      try {
+        await replyMessage.delete();
+      } catch (err) {
+        console.error("Failed to delete interaction reply:", err);
+      }
+    }, 60000);
+
+    interaction.client.once("interactionDelete", () => clearTimeout(deleteTimeout));
+  },
 };
