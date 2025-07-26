ac807c90a6f6cfd349f819bd73ddc0876a317502
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-07-26 13:15:30 -0300

    Update ping.ts

--- dist/commands/ping.ts
+++ dist/commands/ping.ts
@@ -1,30 +1,34 @@
-  import { Embed, Declare, Command, type CommandContext } from "seyfert";
+import { Embed, Declare, Command, type CommandContext } from "seyfert";
 
-  @Declare({
-    name: 'ping',
-    description: 'ping the bot',
-  })
-  export default class pingCmds extends Command {
-      public override async run(ctx: CommandContext): Promise<void> {
-        try {
-          const { client } = ctx;
+@Declare({
+  name: 'ping',
+  description: 'ping the bot',
+})
+export default class pingCmds extends Command {
+  public override async run(ctx: CommandContext): Promise<void> {
+    try {
+      const { client } = ctx;
 
-          const embed = new Embed().setDescription('Pinging...');
+      const embed = new Embed()
 
-          await ctx.editOrReply({ embeds: [embed] });
+      const shardId = ctx.shardId;
 
-          const shardId = ctx.shardId;
+      const wsPing = Math.floor(client.gateway.latency);
+      const shardPing = Math.floor((await ctx.client.gateway.get(shardId)?.ping()) ?? 0);
 
-          const wsPing = Math.floor(client.gateway.latency);
-          const shardPing = Math.floor((await ctx.client.gateway.get(shardId)?.ping()) ?? 0);
-
-          embed
-              .setColor(0)
-              .setDescription(`**Gateway**: ${wsPing}ms\n**Shard**: ${shardPing}ms`);
-
-          await ctx.editOrReply({ embeds: [embed] });
-         } catch (error) {
-           if(error.code === 10065) return;
-        }
+      let playerping = 0;
+      const player = client.aqua.players.get(ctx.guildId);
+      if (player) {
+        playerping = Math.floor(player.ping);
       }
+
+      embed
+        .setColor(0)
+        .setDescription(`**Gateway**: ${wsPing}ms\n**Shard**: ${shardPing}ms\n**Player**: ${playerping}ms`);
+
+      await ctx.write({ embeds: [embed] });
+    } catch (error) {
+      if (error.code === 10065) return;
+    }
   }
+}
