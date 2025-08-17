eef7b3d7a31aa2cf9b019c3a5f4ef53bd10c339d
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-08-17 13:10:36 -0300

    Add tts command

---
+++ dist/commands/tts.ts
@@ -0,0 +1,45 @@
+import { Command, Declare, type CommandContext, Embed, Middlewares, Options, createStringOption } from 'seyfert'
+
+@Options({
+    tts: createStringOption({
+        description: 'Generate and send a TTS message',
+        required: true,
+        max_length: 500,
+        min_length: 1
+    })
+})
+@Middlewares([ 'checkVoice'])
+@Declare({
+    name: 'tts',
+    description: 'Generate and send a TTS message'
+})
+
+export default class TTSCommand extends Command {
+    async run(ctx: CommandContext) {
+        const { tts  } = ctx.options as { tts: string };
+
+        const player = ctx.client.aqua.players.get(ctx.guildId!);
+
+        if (!player) {
+            await ctx.client.aqua.createConnection({
+                guildId: ctx.guildId!,
+                voiceChannel: (await ctx.member?.voice())?.channelId,
+                textChannel: ctx.channelId,
+                deaf: true,
+                defaultVolume: 65
+            })
+            return;
+        }
+
+        if (player) {
+            const resolved = ctx.client.aqua.resolve({
+                query: tts,
+                source: 'speak',
+                requester: ctx.interaction.user
+            })
+            console.log(resolved)
+            player.queue.add((await resolved).tracks[0]);
+            await player.play();
+        }
+    }
+}
